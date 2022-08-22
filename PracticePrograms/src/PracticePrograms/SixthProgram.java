7944dc8f4797bb445b073ad7d9f9688468f183d2
srikanth <srikanthag93@gmail.com>
2022-08-23 01:04:57 +0530

    Commit

---
+++ PracticePrograms/src/PracticePrograms/SixthProgram.java
@@ -0,0 +1,11 @@
+package PracticePrograms;
+
+public class SixthProgram {
+
+	public static void main(String[] args) {
+		String var=new String("HELLO WORLD");
+		System.out.println(var);
+
+	}
+
+}
