14e3c4b0806989578a1339a7e55e7b7936ced109
srikanth <srikanthag93@gmail.com>
2022-08-23 00:03:50 +0530

    Commit

---
+++ Collection_Framework/src/Collection_Framework/HashSetdemo.java
@@ -0,0 +1,25 @@
+package Collection_Framework;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class HashSetdemo {
+
+	public static void main(String[] args) {
+		TreeSet<String> hs=new TreeSet<String>();
+        hs.add("Hash");
+        hs.add("Set");
+        hs.add("Demo");
+        hs.add("Demo");
+        //hs.add(new Integer(10));
+	   
+       Iterator i=hs.iterator();
+       while(i.hasNext())
+       {
+    	   Object o=i.next();
+    	   System.out.println(o);
+       }
+	}
+
+}
