864968d9f4dfe49e59b775e40c7a8a4e4dc612f4
srikanth <srikanthag93@gmail.com>
2022-08-23 01:18:32 +0530

    Commit

---
+++ PracticePrograms/src/PracticePrograms/NinthProgram.java
@@ -0,0 +1,13 @@
+package PracticePrograms;
+
+public class NinthProgram {
+
+	public static void main(String[] args) {
+		
+			while(true){
+			        System.out.println("hello");
+				}
+
+	}
+
+}
