bacd43799bd525d7aa58202d6067c7d2985c5b38
srikanth <srikanthag93@gmail.com>
2022-08-23 00:50:25 +0530

    Commit

---
+++ annoatations/src/annotations/SmartPhone.java
@@ -0,0 +1,14 @@
+package annotations;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SmartPhone {
+		String os() default "Symbian";
+		int version() default 1;	
+	}
